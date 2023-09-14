1ec9795c0a6df3caadb71b1ffb6c56cd15f52bad
Nicolás Muros <nicomuros@gmail.com>
2023-09-13 22:20:14 -0300

    Clase 4, video 1

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,4 +1,33 @@
-package utn.estudiantes.servicio;
+import org.springframework.stereotype.Service;
+import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
 
-public class EstudianteServicio implements iEstudianteServicio{ //esta clase implementa la interface
+import java.util.List;
+
+@Service
+public class EstudianteServicio implements IEstudianteServicio{
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.save(estudiante);
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+        estudianteRepositorio.delete(estudiante);
+    }
 }
