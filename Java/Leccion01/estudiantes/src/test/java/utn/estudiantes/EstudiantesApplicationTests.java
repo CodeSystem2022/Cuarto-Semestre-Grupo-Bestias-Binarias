b481dd38b52701d11146ea0aea7c35e2695686e0
FernandoSilva12 <112596349+FernandoSilva12@users.noreply.github.com>
2023-09-17 20:36:14 -0300

    Update EstudiantesApplicationTests.java
    Java clase 4 videos 4-5-6-7

--- Java/Leccion01/estudiantes/src/test/java/utn/estudiantes/EstudiantesApplicationTests.java
+++ Java/Leccion01/estudiantes/src/test/java/utn/estudiantes/EstudiantesApplicationTests.java
@@ -1,13 +1,31 @@
 package utn.estudiantes;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import utn.estudiantes.servicio.EstudianteServicio;
 
-@SpringBootTest
-class EstudiantesApplicationTests {
+@SpringBootApplication
+class EstudiantesApplicationTests implements CommandLineRunner {
+@Autowired
+private EstudianteServicio estudianteServicio;
+private static final Logger logger = LoggerFactory.getLogger(EstudiantesAplication.class);
 
-	@Test
-	void contextLoads() {
-	}
-
+String nl = System.lineSeparator();
+	
+public static void main (String[] args) {
+	logger.info("Iniciando la aplicacion...");
+	// Levantar la fabrica de Spring
+	SpringApllication.run(EstudiantesApplication.class, args);
+	logger.info("Aplicacion Finalizada!");
+ }
+	
+ @Override
+ public void run(String...args) throws Exception {
+	 logger.info(nl+"Ejecutando el metodo run de Spring.."+nl);
+	 
+ }
 }
