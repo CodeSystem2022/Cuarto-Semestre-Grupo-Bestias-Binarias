ff55f8e6b90f27ee2db4daceec5a8bf93b9e18e3
DaianaEscudero <112595487+DaianaEscudero@users.noreply.github.com>
2023-09-18 14:31:55 -0300

    Agrego codigo clase 3, videos 1-2-3 index.js

--- JavaScripSemana1/e-commerce2023/client/js/index.js
+++ JavaScripSemana1/e-commerce2023/client/js/index.js
@@ -1,11 +1,13 @@
 const shopContent = document.getElementById("shopContent");
+const cart = [];
 
 productos.forEach(product) =>{
     const content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `
     <img src="${product.img}">
     <h3>${product.productName}</h3>
-    <p>${product.price} $</p>
+    <p class="price">${product.price} $</p>
     `;
     shopContent.append(content);
 
@@ -15,14 +17,23 @@ productos.forEach(product) =>{
     content.append(buyButton);
 
     buyBotton.addEventListener("click",()=>{
-        cart.push({
-            id: product.id,
-            productName: product.productName,
-            price: product.price,
-            quanty: product.quanty,
-            img: product.img,
-        })
-        console.log(cart)
-    })
+        const repeat = cart.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if(repeat){
+            cart.map((prod)=> {
+                if(prod.id === product.id){
+                    prod.quanty++;
+                }
+            });
+        }else {
+            cart.push({
+                id: product.id,
+                productName: product.productName,
+                price: product.price,
+                quanty: product.quanty,
+                img: product.img,
+            });
+        }
+    });
 });
 
