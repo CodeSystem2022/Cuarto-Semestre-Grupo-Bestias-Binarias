4298d919257e68de954acb4d7189e0b4bbab2ae9
Carlos David Mato <104330698+MCDavid1972@users.noreply.github.com>
2023-09-18 17:43:46 -0300

    Colecciones Tipo Bool
    Terminado David

--- Clase01/tipo_bool.py
+++ Clase01/tipo_bool.py
@@ -19,3 +19,47 @@ print(f"El valor : {valor}, Resultado: {resultado}")
 valor = "Hola"
 resultado = bool(valor)
 print(f"El valor : {valor}, Resultado: {resultado}")
+
+
+#MCDavid
+# Tipo colecciones -> False para colecciones vacias
+# Tipo colecciones -> True para las demas
+valor =[]
+resultado = bool(valor)
+print(f'valor de una lista vacia: {valor}, Resultado: {resultado}')
+
+valor = [2, 3, 4]
+resultado = bool(valor)
+print(f'valor de una lista con elementos: {valor}, Resultado: {resultado}')
+
+#Tupla
+valor = ()
+resultado = bool(valor)
+print(f'valor de un tupla vacia: {valor}, Resultado: {resultado}')
+
+valor = (5, )
+resultado = bool(valor)
+print(f'valor de una tupla con elementos: {valor}, Resultado: {resultado}')
+
+#Diccionario
+valor = {}
+resultado = bool(valor)
+print(f'valor de un diccionario vacio: {valor}, Resultado: {resultado}')
+
+valor = {'Nombre': 'Juan', 'Apellido': 'Perez'}
+resultado = bool(valor)
+print(f'valor de un diccionario con elementos: {valor}, Resultado: {resultado}')
+
+# Sentencia de control con booleano
+if (1,):
+    print('Regresa verdadero')
+else:
+    print('Regresa falso')
+
+#  uso de  Ciclos
+variable = 17
+while variable:
+    print('Regresa verdadero')
+    break
+else:
+    print('Regresa falso')
