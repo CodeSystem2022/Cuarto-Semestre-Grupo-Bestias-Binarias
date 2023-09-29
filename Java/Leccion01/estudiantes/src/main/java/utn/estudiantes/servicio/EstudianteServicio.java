5571325767acd5b178f26175f9c0574a7577d8cc
Nahuel Abraham Tapia <111470674+NahuelTapia@users.noreply.github.com>
2023-09-29 20:24:43 -0300

    Update EstudianteServicio.java

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -10,24 +10,24 @@ public class EstudianteServicio implements IEstudianteServicio{
     private EstudianteRepositorio estudianteRepositorio;
 
     @Override
-    public List<Estudiante> listarEstudiantes() {
-        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+    public List<Estudiante2022> listarEstudiantes() {
+        List<Estudiante2022> estudiantes = estudianteRepositorio.findAll();
         return estudiantes;
     }
 
     @Override
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
-        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante2022) {
+        Estudiante2022 estudiante = estudianteRepositorio.findById(idEstudiante2022).orElse(null);
         return estudiante;
     }
 
     @Override
-    public void guardarEstudiante(Estudiante estudiante) {
+    public void guardarEstudiante(Estudiante2022 estudiante) {
         estudianteRepositorio.save(estudiante);
     }
 
     @Override
-    public void eliminarEstudiante(Estudiante estudiante) {
+    public void eliminarEstudiante(Estudiante2022 estudiante) {
         estudianteRepositorio.delete(estudiante);
     }
 }
