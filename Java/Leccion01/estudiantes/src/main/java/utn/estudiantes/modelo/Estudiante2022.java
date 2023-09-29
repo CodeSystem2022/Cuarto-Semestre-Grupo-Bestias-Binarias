1863be51cc1ceb0794d05180120e1e546d5af835
Nahuel Abraham Tapia <111470674+NahuelTapia@users.noreply.github.com>
2023-09-29 20:20:21 -0300

    Agrego Clase05-video1

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante2022.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante2022.java
@@ -16,12 +16,13 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 
-public class Estudiante {
+public class Estudiantes2022 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer idEstudiante;
+    private  Integer idestudiantes2022;
     private String nombre;
     private String apellido;
     private String telefono;
     private String email;
 }
+
