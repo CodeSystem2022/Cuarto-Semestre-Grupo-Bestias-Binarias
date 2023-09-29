d880837307bb159a91fc77bdf55d83ff830d1e71
Nahuel Abraham Tapia <111470674+NahuelTapia@users.noreply.github.com>
2023-09-29 20:22:00 -0300

    Update IEstudianteServicio.java

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -1,6 +1,6 @@
 package utn.estudiantes.servicio;
 
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiante2022;
 
 import java.util.List;
 
@@ -8,8 +8,8 @@ public interface iEstudianteServicio {
     // va a tener los métodos básicos para trabajar con la entidad Estudiante
     // usando la interface repositorio
     public List<Estudiante> listarEstudiantes();
-    public Estudiante buscarEstudianteporId(Integer idEstudiante);
-    public void guardarEstudiante(Estudiante estudiante);
-    public void eliminarEstudiante(Estudiante estudiante);
+    public Estudiante2022 buscarEstudianteporId(Integer idEstudiante2022);
+    public void guardarEstudiante(Estudiante2022 estudiante);
+    public void eliminarEstudiante(Estudiante2022 estudiante);
 
 }
