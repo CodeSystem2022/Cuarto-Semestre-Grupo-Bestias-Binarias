4cb20dd6490ece37b8313a8c0849d62eb0779956
Nicolás Muros <nicomuros@gmail.com>
2023-10-01 18:56:22 -0300

    Clase 5 y 6

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -53,7 +53,58 @@ public class EstudiantesApplication implements CommandLineRunner {
 				logger.info("Listando Estudiantes: "+nl);
 				List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
 				estudiantes.forEach(estudiante -> logger.info(estudiante.toString()+nl));
+            }//Fin caso 1
+            case 3 -> { //Agregar estudiante
+				logger.info("Ingrese los datos del estudiante: ");
+				logger.info("Nombre: ");
+				var nombre = entrada.nextLine();
+				logger.info("Apellido: ");
+				var apellido = entrada.nextLine();
+				logger.info("Teléfono: ");
+				var telefono = entrada.nextLine();
+				logger.info("email: ");
+				var email = entrada.nextLine();
 
+				var estudiante = new Estudiantes2022();
+				estudiante.setNombre(nombre);
+				estudiante.setApellido(apellido);
+				estudiante.setEmail(email);
+				estudiante.setTelefono(telefono);
+
+				estudianteServicio.guardarEstudiante(estudiante);
+				logger.info("Estudiante agregado: " + estudiante + nl);
+				return false;
+			}
+			case 4 -> { //Modificar estudiante
+				logger.info("Modificando estudiante: ");
+				var idEstudiante = Integer.parseInt(entrada.nextLine());
+				Estudiantes2022 estudiante = estudianteServicio.buscarEstudiantePorId(idEstudiante);
+
+				if (estudiante != null){
+					logger.info("Ingrese los nuevos datos del estudiante: ");
+					var nombre = entrada.nextLine();
+					var apellido = entrada.nextLine();
+					var telefono = entrada.nextLine();
+					var email = entrada.nextLine();
+
+					estudiante.setNombre(nombre);
+					estudiante.setApellido(apellido);
+					estudiante.setEmail(email);
+					estudiante.setTelefono(telefono);
+
+					estudianteServicio.guardarEstudiante(estudiante);
+					return false;
+				} else {
+					logger.info("Error en el número de id. Saliendo al menú...");
+					return true;
+				}
+			}
+			case 5 -> { //Eliminar estudiante
+				logger.info("Ingrese el ID: ");
+				var idEstudiante = Integer.parseInt(entrada.nextLine());
+				Estudiantes2022 estudiante = estudianteServicio.buscarEstudiantePorId(idEstudiante);
+				estudianteServicio.eliminarEstudiante(estudiante);
+			}
 
 
 
