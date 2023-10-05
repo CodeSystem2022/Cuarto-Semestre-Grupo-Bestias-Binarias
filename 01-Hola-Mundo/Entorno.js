6b9d024fc5bfbcfbf81b7354b67aab3adcae477f
FernandoSilva12 <112596349+FernandoSilva12@users.noreply.github.com>
2023-10-04 21:00:05 -0300

    Update Entorno.js
    Clase 7 Node, nodemon/pm2

--- 01-Hola-Mundo/Entorno.js
+++ 01-Hola-Mundo/Entorno.js
@@ -4,3 +4,12 @@ let web = process.env.MI_WEB || 'No tengo web';
 
 console.log('Hola'+nombre);
 console.log('Mi web es: '+web);
+
+console.log('Ultima ejecucion');
+
+//Importante, para instalar nodemon, se usa el comando :
+//npm install -g nodemon
+//Si nos tira error, se soluciona en powershield con los comandos
+//Get-ExecutionPolicy -List
+//Set-ExecutionPolicy RemoteSigned -Scope CurrentUser ​
+// para instalar pm2 npm install pm2 -g
