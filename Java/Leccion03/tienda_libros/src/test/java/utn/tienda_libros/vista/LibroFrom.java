6d94edfef1f64a6c8f535577e44e250ef9a09ff5
Nahuel Abraham Tapia <111470674+NahuelTapia@users.noreply.github.com>
2023-10-25 19:18:56 -0300

    Agrego clase 8, video 7

--- Java/Leccion03/tienda_libros/src/test/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Leccion03/tienda_libros/src/test/java/utn/tienda_libros/vista/LibroFrom.java
@@ -20,4 +20,23 @@ public class LibroFrom extends JFrame {
         setSize(900, 700);
     }
 
+     private void listarLibros(){
+        // Limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+        // Obtener los libros de la BD
+        var libros = libroServicio.listarLibros();
+        //Iteramos cada libro
+        libros.forEach((libro)->{
+            //Creamos cada registro para agregarlo a la tabla
+            Object[] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
+    }
+
 }
