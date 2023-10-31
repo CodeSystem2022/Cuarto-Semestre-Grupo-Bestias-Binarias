542e031a5e06ff0acce37b52092e80bde9dc90f1
Fernando Silva <112596349+FernandoSilva12@users.noreply.github.com>
2023-10-30 22:50:24 -0300

    Clase 9 video 1,2,3
    Agrego clase 9 video 1,2,3

---
+++ Fundamentos_node/async/callbackHell.js
@@ -0,0 +1,51 @@
+function hola(nombre, miCallback){
+    setTimeout(function () {
+        console.log('Hola '+nombre);
+        miCallback(nombre)
+    }, 1000);
+    
+}
+
+function hablar(callbackHablar){
+    setTimeout(function (){
+        console.log('bla bla bla')
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout( function() {
+        console.log('Adios ' + nombre);
+        otroCallback();
+    }, 1500);
+}
+
+function conversacion(nombre, veces, callback){
+    hablar(function(){
+        conversacion();
+    })
+}
+
+
+// ----Proceso Principal
+
+console.log('Iniciando el proceso...');
+hola('Ariel', adios); {
+//    adios(nombre, function() {
+//        console.log('Terminamos')
+//    });
+//});
+    //    hablar( function(){
+//        hablar( function() {
+//            hablar( function() {
+//                hablar( function() {
+//                    adios(nombre, function(){
+  //                      console.log('Terminando el proceso...');
+    //             });
+      //      });
+        //});
+    // });
+     
+    //});
+//});
+
