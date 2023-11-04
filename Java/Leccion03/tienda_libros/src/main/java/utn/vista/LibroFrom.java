ad6f640252877d891f2e3d165a32a6da50e09633
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-11-03 22:07:57 -0300

    agrego clase 10

--- Java/Leccion03/tienda_libros/src/main/java/utn/vista/LibroFrom.java
+++ Java/Leccion03/tienda_libros/src/main/java/utn/vista/LibroFrom.java
@@ -135,14 +135,7 @@ public class LibroFrom extends JFrame {
         if (renglon != -1) {
             String idLibro = tablaLibros.getModel().getValueAt(renglon, 0).toString();
             idTexto.setText(idLibro);
-            String nombreLibro = tablaLibros.getModel().getValueAt(renglon, 1).toString();
-            libroTexto.setText(nombreLibro);
-            String autor = tablaLibros.getModel().getValueAt(renglon, 2).toString();
-            autorTexto.setText(autor);
-            String precio = tablaLibros.getModel().getValueAt(renglon, 3).toString();
-            precioTexto.setText(precio);
-            String existencias = tablaLibros.getModel().getValueAt(renglon, 4).toString();
-            existenciasTexto.setText(existencias);
+          
         }
     }
 
