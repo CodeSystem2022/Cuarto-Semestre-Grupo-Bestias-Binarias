c42edb965537f0f12538edd6458ccdfcc9951f0c
Daniel Alejandro Guerrero <50740422+DanielGuerrero03@users.noreply.github.com>
2023-11-06 19:21:36 -0300

    Update promises.js hasta clase 10 video 5

--- Fundamentos_node/async/promises.js
+++ Fundamentos_node/async/promises.js
@@ -1,27 +1,39 @@
 function hola(nombre) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            console.log('Hola ' + nombre);
+    return new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            console.log("Hola " +nombre);
             resolve(nombre);
         }, 1000);
     });
 }
 
 function hablar(nombre) {
-    return new Promise(function (resolve, reject) {
-        setTimeout(function () {
-            console.log('bla bla bla');
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log("Bla bla bla bla...");
             resolve(nombre);
         }, 1000);
     });
 }
-function adios(nombre, otroCallback) {
-    setTimeout( function() {
-        console.log('Adios ' + nombre);
-        otroCallback();
-    }, 1500);
+
+function adios(nombre) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function() {
+            console.log("Adios " +nombre);
+            //resolve();
+            reject("Hay un error");
+        }, 1000);
+    });
 }
-hola ('Ariel')
-.then((nombre) =>{
-    console.log('Terminando el proceso');
-})
+
+// Llama a la funcion hola
+console.log("Iniciando proceso...");
+hola("Daniel")
+    .then(hablar)
+    .then(hablar)
+    .then(hablar)
+    .then(adios)
+    .then((nombre) => {
+        console.log("Terminando proceso...");
+    })
+   
